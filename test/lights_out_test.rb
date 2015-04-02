6be81e1c06c0892807b4a2fd7725f2c3286172ea
David Eisinger <david.eisinger@gmail.com>
2015-04-02 11:30:45 -0400

    Add more test cases + timeout

--- test/lights_out_test.rb
+++ test/lights_out_test.rb
@@ -1,21 +1,32 @@
+require 'timeout'
 require "minitest/autorun"
 
 class LightsOutTest < MiniTest::Test
+  include Timeout
+
   CASES = {
     "000000000" => 0,
     "010111010" => 1,
-    "110001101" => 3
+    "110001101" => 3,
+    "000000001" => 5,
+    "100000101" => 8,
+    "101010101" => 9
   }
 
   CASES.each do |board, moves|
     define_method "test_solution_to_#{board}" do
-      solution = `#{ENV["LANGUAGE_PATH"]}/lights_out #{board}`
-      assert_equal "000\n000\n000", `./runner.rb #{board} #{solution}`.strip
+      timeout 20 do
+        solution = `#{ENV["LANGUAGE_PATH"]}/lights_out #{board}`
+        assert_equal "000\n000\n000", `./runner.rb #{board} #{solution}`.strip
+      end
     end
 
     define_method "test_length_of_solution_to_#{board}" do
-      solution = `#{ENV["LANGUAGE_PATH"]}/lights_out #{board}`.strip
-      assert solution.length <= moves, "Solution to #{board} (#{solution}) longer than #{moves}"
+      timeout 20 do
+        solution = `#{ENV["LANGUAGE_PATH"]}/lights_out #{board}`.strip
+        assert solution.length <= moves,
+          "Solution to #{board} (#{solution}) longer than #{moves}"
+      end
     end
   end
 end
